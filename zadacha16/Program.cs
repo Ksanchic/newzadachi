d8f1ed360e9d8e6ef9a2586cfeb7077c33d19afc
ksanchic <ksanchic@mail.ru>
2022-12-06 01:20:39 +0300

    zadacha16

---
+++ zadacha16/Program.cs
@@ -0,0 +1,20 @@
+﻿// программа,которая принимает на вход 2 числа и проверяет, является 
+// ли одно число квадратом другого 
+{
+int a;
+int b;
+Console.WriteLine ("Введите два числа: ");
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+
+
+  if ((a * a == b)| (b * b == a))
+  {
+    Console.WriteLine ("да");
+  }
+ 
+else
+{
+    Console.WriteLine ("нет");
+}
+}
