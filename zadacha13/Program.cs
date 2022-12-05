e895c2f6286d08102f1b1bdcd93cee3c8c4d4014
ksanchic <ksanchic@mail.ru>
2022-12-06 01:13:13 +0300

    zadacha13

---
+++ zadacha13/Program.cs
@@ -0,0 +1,16 @@
+﻿// программа, которая выводит третью цифру заданного 
+//числа или сообщает, что третьей цифры нет
+
+{
+    int num;
+Console.WriteLine ("Введите число");
+string a = Console.ReadLine ();
+
+ if (a.Length <=2)
+ {
+    Console.WriteLine ("Третьей цифры нет");
+ }
+ else 
+ 
+      Console.WriteLine ("Третья цифра: " +a[2]);
+ }
